0b07efe402606df948e14d330e7f2a457fbbf07c
momogc <172766829@qq.com>
2018-04-07 15:17:14 +0800

    readme

---
+++ config/default.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: 3002,
+    name: 'homeserver',
+    staticpath: '../myhomepage/dist'
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const config = require('config-lite')(__dirname)
+const path = require('path')
+const favicon = require('express-favicon')
+var app = express()
+app.get('/', function (req, res) {
+    res.sendFile((path.resolve(__dirname, config.staticpath)) + '/index.html')
+})
+app.use(favicon(path.join(__dirname,'/favicon.ico')))
+app.use(express.static(path.resolve(__dirname, config.staticpath)))
+app.listen(config.port, function () {
+    console.log(`${config.name} listening on port ${config.port}`)
+})
